c0a292a521a91226cd02b0a5e42ee632e94119bf
yunjion <yunjion37@gmail.com>
2026-04-15 12:09:28 +0900

    자바스크립트 실습

---
+++ source/js/common.js
@@ -0,0 +1,20 @@
+  /**
+   * 두 수를 다해서 결과를 리턴하는 함수 
+   * @param {*} n1 : 더하고자 하는 수1
+   * @param {*} n2 : 더하고자 하는 수2
+   * @returns 
+   */
+    const sum = (n1,n2) => n1 + n2;
+     //   const sum3= (n1,n2) => n1+n2;
+         //console.log(sum3(4,5));
+        /**
+   * 부가세를 계산하는 함수 
+   * @param {*} productPrice : 물품가격 
+   * @returns 
+   */
+    const taxAmount = (productPrice) => {
+    let tax = 0.1;
+    return tax * productPrice;
+};
+        
+    
